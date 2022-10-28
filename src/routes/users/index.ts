7b252725f6bd5d35923e8ac030efee11cf08a033
Yoan <yoansjpro@gmail.com>
2022-10-28 12:32:16 +0200

    WIP Prisma

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,7 +1,27 @@
 import { FastifyPluginAsync } from 'fastify';
 
-const users: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (request, reply) => 'this is an example');
+const users: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.post<{
+    Body: {
+      name: string;
+      email: string;
+      password: string;
+    };
+  }>('/', async (request) => {
+    try {
+      await fastify.prisma.user.create({
+        data: {
+          name: request.body.name,
+          email: request.body.email,
+          password: request.body.password,
+        },
+      });
+
+      return { success: true };
+    } catch (error) {
+      return { success: false };
+    }
+  });
 };
 
 export default users;
