24b74a45dc565e397654ab472593086d4df7278e
Yoan <yoansjpro@gmail.com>
2022-11-04 15:28:31 +0100

    Add Typescript route verify

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,7 +1,18 @@
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { FastifyPluginAsync } from 'fastify';
+import { User, UserType } from '../../schemas/User';
 
 const users: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (request, reply) => 'this is an example');
+  fastify
+    .withTypeProvider<TypeBoxTypeProvider>()
+    .post<{ Body: UserType; Reply: UserType }>(
+      '/',
+      { schema: { body: User, response: { 200: User } } },
+      async (request, reply) => {
+        const { name, mail } = request.body;
+        reply.status(200).send({ name, mail });
+      },
+    );
 };
 
 export default users;
